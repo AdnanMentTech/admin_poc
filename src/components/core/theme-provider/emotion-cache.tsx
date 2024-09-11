5800e8d8013b02c8300a33dae3894653703515e0
1 <a1@MacBook-Pro.local>
2024-09-11 17:01:38 +0530

    Updates

--- src/components/core/theme-provider/emotion-cache.tsx
+++ src/components/core/theme-provider/emotion-cache.tsx
@@ -61,9 +61,9 @@ export default function NextAppDirEmotionCacheProvider(props: NextAppDirEmotionC
 
       if (typeof style !== 'boolean') {
         if (isGlobal) {
-          globals.push({ name, style });
+          globals.push({ name, style: style ?? '' });
         } else {
-          styles += style;
+          styles += style ?? '';
           dataEmotionAttribute += ` ${name}`;
         }
       }
